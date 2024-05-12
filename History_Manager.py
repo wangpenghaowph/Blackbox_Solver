87e0cf999eebe083d987307cdf3015b9a7a77261
wangpenghaowph <20307130039@fudan.edu.cn>
2024-05-12 23:52:00 +0000

    Add comments in History_Manager.py

--- History_Manager.py
+++ History_Manager.py
@@ -1,5 +1,28 @@
 # This module records all the necessary history information
+# Any function that inputs/outputs the history information should be defined in this module
+"""
+Functions in this module:
+****evaluate(x, obj_fun, iter, stage)
+x is a list(a vector) or a list of lists(multiple vectors)
+(e.g. x=[x1,x2,x3] or x=[[x1,x2,x3],[x4,x5,x6],[x7,x8,x9])
+obj_fun is the objective function
+iter is the current iteration number
+stage is in ['EG',DS','TR'] (Estimate Gradient, Direct Search, Trust Region)
 
+****get_momentum_direction(n_m)
+n_m is the number of momentum directions
+
+****find_best_per_iter(iter)
+
+****evaluate_grad(x, grad_fun, iter, stage)
+grad_fun is the gradient function
+
+****get_nfev()
+Get the value of nfev. Used when the solver terminates.
+
+****get_ngrad()
+Get the value of ngrad. Used when the solver terminates.
+"""
 class History_Manager:
     def __init__(self, obj_fun, grad_fun=None):
         self.obj_fun = obj_fun
@@ -39,10 +62,14 @@ class History_Manager:
         if best_entry:
             self.iter_history[iter] = best_entry
         return best_entry
+    def get_momentum_direction(self, n_m):
+        #TODO:
+        pass
     def evaluate_grad(self, x, grad_fun, iter, stage):
         self.ngrad += 1
         grad = grad_fun(x)
         self.total_history[(iter, stage)]["gradient"] = grad
+        return grad
 
     def get_nfev(self):
         return self.nfev
