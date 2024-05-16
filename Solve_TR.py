36abe97bb344aafb62c16b5f4113cf35592c23b0
wangpenghaowph <20307130039@fudan.edu.cn>
2024-05-16 12:06:31 +0000

    Save current changes

--- Solve_TR.py
+++ Solve_TR.py
@@ -1,7 +1,11 @@
 import numpy as np
 
-def Solve_TR(model, n, tr_radius, method_solve_tr):
+def Solve_TR(H, g, tr_radius, method_solve_tr):
     tr_sol = []
+    """
+    model is a quadratic function, solve for the hessian and gradient
+    """
+    
     if method_solve_tr == 'dogleg':
         # TODO: Implement dogleg method
         pass
